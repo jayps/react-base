d88e97fb5dfa8b1381a5c622adfbd3b62ad7a819
JP Meyer <jp@jpmeyer.dev>
2024-09-16 16:52:44 +0200

    Added loading to login page.

--- src/components/forms/login/index.tsx
+++ src/components/forms/login/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Button from '../../button';
 import {SubmitHandler, useForm} from 'react-hook-form';
 import {Link} from 'react-router-dom';
+import Alert from '../../alert';
 
 type LoginFormInputs = {
     email: string;
@@ -9,18 +10,53 @@ type LoginFormInputs = {
 }
 
 const LoginForm: React.FC = () => {
+    const [isLoggingIn, setIsLoggingIn] = React.useState(false);
+    const [errorMessage, setErrorMessage] = React.useState<string | undefined>();
+    const [success, setSuccess] = React.useState(false);
     const {
         register,
         handleSubmit,
         watch,
-        formState: {errors},
+        formState: {errors, isValid},
     } = useForm<LoginFormInputs>();
 
-    const onSubmit: SubmitHandler<LoginFormInputs> = (data) => console.log(data)
+    const onSubmit: SubmitHandler<LoginFormInputs> = async (data) => {
+        setIsLoggingIn(true);
+        setErrorMessage(undefined);
+        setSuccess(false);
+        try {
+            const httpResponse = await fetch(`${process.env.REACT_APP_API_BASE_URL}/auth/login/`, {
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            const response = await httpResponse.json();
+            if (httpResponse.status !== 200) {
+                if (httpResponse.status === 401) {
+                    setErrorMessage('Incorrect e-mail address or password.')
+                } else if (response.data.detail) {
+                    setErrorMessage(response.data.detail);
+                } else {
+                    setErrorMessage('An error occurred. Please try again.')
+                }
+            } else {
+                setSuccess(true);
+                console.log(response);
+                alert('Logged in!')
+            }
+        } catch (err) {
+            console.error(err);
+            setErrorMessage('An error occurred. Please try again.')
+        }
+
+        setIsLoggingIn(false);
+    }
 
     return (
         <>
-            <div className="login-form rounded bg-white p-5 min-w-96 min-h-96 flex flex-col justify-center items-center">
+            <div className="login-form shadow-xl rounded bg-white p-5 min-w-96 min-h-96 flex flex-col justify-center items-center">
                 <div className="min-w-96">
                     <h2>Login</h2>
                 </div>
@@ -40,8 +76,14 @@ const LoginForm: React.FC = () => {
                                 errors.password && <span>This field is required</span>
                             }
                         </div>
+                        {
+                            errorMessage && (
+                                <Alert severity="error">{errorMessage}</Alert>
+                            )
+                        }
                         <div className="text-end flex flex-col justify-end">
-                            <Button text="Login" type="submit" color="primary" className="my-5"/>
+                            <Button text="Login" type="submit" color="primary" className="my-5" disabled={!isValid}
+                                    busy={isLoggingIn}/>
                             <Link to="/register">New User? Register here.</Link>
                         </div>
                     </form>

c1668312f55f182a4feb015a12d780337385ef2d
JP Meyer <jp@jpmeyer.dev>
2024-09-19 09:28:43 +0200

    Adjusted button to support links, onclicks or neither (i.e. submit)

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 export type ButtonProps = {
     text: string;
     type?: 'button' | 'submit';
     id?: string;
     onClick?: () => void;
+    link?: string;
     color?: 'primary' | 'secondary';
     className?: string;
     disabled?: boolean;
@@ -16,23 +18,49 @@ const Button: React.FC<ButtonProps> = ({
                                            type,
                                            id,
                                            onClick,
+                                           link,
                                            color,
                                            className,
                                            disabled,
                                            busy
                                        }) => {
     const computedClassName = `${color || 'secondary'} ${className}`;
-    return (
-        <button className={computedClassName} onClick={onClick} id={id} type={type || 'button'}
-                disabled={disabled || busy || false}>
-            {
-                busy && (
-                    <div className="loader"></div>
-                )
-            }
-            <span>{text}</span>
-        </button>
-    )
+    if (onClick) {
+        return (
+            <button className={computedClassName} onClick={onClick} id={id} type={type || 'button'}
+                    disabled={disabled || busy || false}>
+                {
+                    busy && (
+                        <div className="loader"></div>
+                    )
+                }
+                <span>{text}</span>
+            </button>
+        )
+    } else if (link) {
+        return (
+            <Link to={link} className={computedClassName} id={id}>
+                {
+                    busy && (
+                        <div className="loader"></div>
+                    )
+                }
+                <span>{text}</span>
+            </Link>
+        )
+    } else {
+        return (
+            <button className={computedClassName} id={id} type={type || 'button'}
+                    disabled={disabled || busy || false}>
+                {
+                    busy && (
+                        <div className="loader"></div>
+                    )
+                }
+                <span>{text}</span>
+            </button>
+        );
+    }
 }
 
 export default Button;

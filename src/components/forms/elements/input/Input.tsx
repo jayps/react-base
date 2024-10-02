c8a97cd7640981c21ae4f3bf27714452f92375b0
JP Meyer <jp@jpmeyer.dev>
2024-10-02 07:34:57 +0200

    Improved error message computation for inputs.

--- src/components/forms/elements/input/Input.tsx
+++ src/components/forms/elements/input/Input.tsx
@@ -12,12 +12,31 @@ export interface InputProps<T extends FieldValues> {
 }
 
 const Input: React.FC<InputProps<any>> = ({name, label, type = 'text', required = false, errors, register, validate}) => {
+    const getError = () => {
+        const defaultMessage = 'Please check your input.'
+        if (errors && errors[name]) {
+            const errorType = errors[name]?.type as string;
+            switch (errorType.toLowerCase()) {
+                case 'required':
+                    return 'This field is required.';
+                case 'custom':
+                    return errors[name]?.message as string || defaultMessage;
+                case 'validate':
+                    return '';
+                default:
+                    return defaultMessage;
+            }
+        }
+
+        return defaultMessage;
+    }
+
     return (
         <div className="form-group">
             <label htmlFor={name}>{label}</label>
             <input type={type} {...register(name, {required, validate})} />
             {
-                errors && errors[name] && <span className="input-error">This field is required</span>
+                errors && errors[name] && <span className="input-error">{getError()}</span>
             }
         </div>
     )

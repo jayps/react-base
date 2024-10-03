768bdac8f44a0691b53137441b0dcd9b8b5e074b
JP Meyer <jp@jpmeyer.dev>
2024-10-03 20:30:32 +0200

    Write checkbox error case.

--- src/components/forms/elements/checkbox/index.test.tsx
+++ src/components/forms/elements/checkbox/index.test.tsx
@@ -49,4 +49,43 @@ describe('Checkbox', () => {
         await new Promise(process.nextTick);
         expect(mockSubmit).toHaveBeenCalled();
     });
+
+    it('should show error when required and unchecked', async () => {
+        const mockSubmit = jest.fn(({testCheckbox}) => {
+            console.log(testCheckbox)
+        });
+
+        const TestForm: React.FC = () => {
+            const {
+                handleSubmit,
+                control,
+                formState: {errors}
+            } = useForm<{ testCheckbox: boolean }>();
+
+            return (
+                <form onSubmit={handleSubmit(mockSubmit)}>
+                    <Checkbox name="testCheckbox" errors={errors} control={control} required={true}/>
+                    <button type="submit" data-testid="test-button">Submit</button>
+                </form>
+            )
+        }
+        
+        render(<TestForm/>)
+
+        // Get checkbox and check it
+        const checkboxElement = screen.getByTestId('checkbox');
+        // Check that checkbox is not checked
+        expect(checkboxElement).not.toBeChecked();
+
+        // Submit form
+        const submitButtonElement = screen.getByTestId('test-button');
+        fireEvent.submit(submitButtonElement);
+
+        // IMPORTANT: Wait for next tick... Allows async processes like form submission to complete.
+        await new Promise(process.nextTick);
+        expect(mockSubmit).not.toHaveBeenCalled();
+        const errorElement = await screen.findByTestId('checkbox-error');
+        expect(errorElement).not.toBeNull();
+        expect(errorElement).toHaveTextContent('This field is required.');
+    });
 })
